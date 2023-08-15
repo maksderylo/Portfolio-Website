25e3e0ea54c341878cf8b63a1eefeaf71775947a
Maks <derylomaksoff@gmail.com>
2023-08-15 11:40:03 +0200

    grid edit

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ const About = (props) => {
         <div className="cont">
         <div className='textb'>  
             <h1>Hello</h1>
-            <h2>I'm Maks Deryło.<br/>Software Developer &<br/> Computer Science Student</h2>      
+            <h2>I'm Maks Deryło.<br/>Software Developer &<br/> Computer Science Student at TU/E</h2>      
         </div>
         <div className='forimg'>
                 <img id='aboutintr' src={aboutintr} alt='image of me'></img>
@@ -36,26 +36,29 @@ const About = (props) => {
         </div>
 
         <div id='sections'>
-
+        <h1>Professional Timeline</h1>
         <section>
         <div className='grid'>
         <article>
-        <h3>9:00 AM</h3>
-        <p>Life finds a way. You know what? It is beets. </p>
+        <h3>Giganci Programowania Course</h3>
+        <h4>June 2017 – July 2017</h4>
+        <p>Developed C# and HTML programming skills, used HTML, CSS, and JavaScript to create project websites. </p>
       </article>
       <article>
-        <h3>10:00 AM</h3>
-        <p>I've crashed into a beet truck </p>
+        <h3>SAP Cybersecurity Virtual Internship Program</h3>
+        <h4>June 27th, 2022</h4>
+        <p>Finished virtual internship program for SAP USA on cybersecurity, worked on industry analysis and anti-phishing tactics.</p>
       </article>
       <article>
-        <h3>12:30 AM</h3>
-        <p>I was part of something special. </p>
+        <h3>Club of Young Scientists</h3>
+        <h4>Feb. 2019 – June 2019</h4>
+        <p>Course in the field of computer science and physics made by Warsaw Higher School of Computer Science, interdisciplinary classes with a group project - a website describing particular celestial bodies.</p>
       </article>
       <article>
         <h3>13:30 AM</h3>
         <p>Yeah, but your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should. </p>
         <img src="https://images.fineartamerica.com/images-medium-large-5/maroon-bells-aspen-colorado-black-and-white-photography-by-sai.jpg" alt="Black and white photo of a lake"/>
-        </article>
+    </article>
         <article>
         <h3>14:30 AM</h3>
         <p>Just my luck, no ice. God help us, we're in the hands of engineers. </p>
